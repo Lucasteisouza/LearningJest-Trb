862736d72e2a2adf5753db0f5d426af3d332b799
Lucasteisouza <lucasteisouza@gmail.com>
2022-09-20 16:41:45 -0300

    Requisito 5 atendido

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (string) => {
+  const obj = {
+    name: string,
+    feedback() { return 'Eita pessoa boa!'; },
+  };
+  return obj;
+};
 module.exports = createStudent;
